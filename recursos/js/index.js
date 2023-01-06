4aedca443bb6c503ffc722db153f1a28011ae82f
Alesxanderk <adonis.ramos.12@gmail.com>
2023-01-05 22:49:36 -0600

    Add files via upload

--- recursos/js/index.js
+++ recursos/js/index.js
@@ -2,7 +2,6 @@ window.onbeforeunload=detener_tts;
 document.onfullscreenchange=pantalla_salida;
 
 /* Carga de archivo */
-
 let elem_archivo=document.createElement('input');
 elem_archivo.setAttribute('type','file');
 elem_archivo.setAttribute('onchange','cargar_epub()');
@@ -11,26 +10,37 @@ let lector;
 let archivo;
 
 /* Elemntos */
-
-let elem_modal_ajustes_tts;
-let elem_modal_libros;
-let elem_encabezado;
-let elem_visor;
-let elem_contenido;
-let elem_valor_paginas;
-let elem_rango_paginas;
-let elem_rango_burbuja;
-let elem_valor_progeso;
-let elem_botones_paginas;
-let elem_listado_libros;
-let elem_listado_voces_tts;
-let elem_estilos_extra;
+let estilos_extra;
+let abrir_1;
+let abrir_2;
+let pantalla_1;
+let pantalla_2;
+let boton_izq;
+let boton_der;
+let boton_tts;
+let tts_1;
+let tts_2;
+let contenedor_rango;
+let rango_paginas;
+let valor_paginas;
+let visor;
+let contenido;
+let modal_tts_ajustes;
+let contenido_tts_ajustes;
+let rate;
+let pitch;
+let listado_voces_tts;
+let modal_libros;
+let listado_libros;
+let contenido_libros;
 
 /* JSZip */
-
 let zip=new JSZip();
+
+/* Parser */
 let parser=new DOMParser();
 
+/* -- */
 let dir_estilos_extra;
 let dir_paginas;
 let dir_imagenes;
@@ -43,134 +53,59 @@ let max_parrafos;
 let max_scroll=0;
 
 /* TTS */
-
 let tts=window.speechSynthesis;
 let utterance=new SpeechSynthesisUtterance();
 let voces=[];
 let cancelado=false;
 
 /* db */
-
 let db=window.localStorage;
 let pagina_cargada=null;
 let nombre_archivo='';
 
-/* JsStore */
-
-/*let connection=new JsStore.Connection();
-
-async function iniciar_index_db(){
-	var dbName ='JsStore_Demo';
-	var tblProduct = {
-		name: 'Product',
-		columns: {
-			// Here "Id" is name of column 
-			id:{ primaryKey: true, autoIncrement: true },
-			itemName:  { notNull: true, dataType: "string" },
-			price:  { notNull: true, dataType: "number" },
-			quantity : { notNull: true, dataType: "number" }
-		}
-	};
-	var database = {
-		name: dbName,
-		tables: [tblProduct]
-	}
-	
-	const isDbCreated = await connection.initDb(database);
-	if(isDbCreated === true){
-		console.log("db created");
-		// here you can prefill database with some data
-	}
-	else {
-		console.log("db opened");
-	}  
-};
-
-async function agregar_datos(){
-	var value = {
-		itemName: 'Blue Jeans',
-		price: 2000,
-		quantity: 1000
-	};
-	var insertCount = await connection.insert({
-		into: 'Product',
-		values: [value]
-	});
-
-	console.log(`${insertCount} rows inserted`);
-};
-
-async function leer_datos(){
-	// results will be array of objects
-	var results = await connection.select({
-		from: 'Product',
-		where: {
-			id: 5
-		}
-	});
-	alert(results.length + 'record found');
-};
-
-async function actualizar_datos(){
-	var rowsUpdated = await connection.update({ 
-		in: 'Product',
-		where: {
-			itemName: {
-				like: '%black%'
-			}
-		},
-		set: {
-			quantity: 2000
-		}
-	});
-	alert(rowsUpdated + ' rows updated');
-};
-
-async function borrar_datos(){
-	var rowsDeleted = await connection.remove({
-		from: 'Product',
-		where: {
-			id: 10
-		}
-	});
-	alert(rowsDeleted + ' record deleted');
-};*/
-
 /* Controles menu */
-
 let libro_abierto=false;
 
-/* Funciones */
+/* Control color tema */
+let color_tema=1;
 
+/* Funciones */
 function cargar(){
 	buscar_elementos();
 	comprobar_tts();
 	listado_voces();
 	preparar_utterance();
-	//iniciar_index_db();
 };
 
 /* Carga de elementos */
-
 function buscar_elementos(){
-	elem_visor=document.getElementById('visor');
-	elem_contenido=document.getElementById('contenido');
-	elem_modal_ajustes_tts=document.getElementById('modal_tts_ajustes');
-	elem_modal_libros=document.getElementById('modal_libros');
-	elem_valor_paginas=document.getElementById('valor_pagina');
-	elem_rango_paginas=document.getElementById('rango_paginas');
-	elem_valor_progeso=document.getElementById('valor_progreso');
-	elem_listado_libros=document.getElementById('listado_libros');
-	elem_listado_voces_tts=document.getElementById('listado_voces_tts');
-	elem_pitch=document.getElementById('pitch');
-	elem_rate=document.getElementById('rate');
-	elem_estilos_extra=document.getElementById('externo');
-	elem_botones_paginas=document.getElementById('botones_paginas');
-	elem_rango_paginas.addEventListener('input',tooltip_range);
+	estilos_extra=document.getElementById('externo');
+	abrir_1=document.getElementById('abrir_1');
+	abrir_2=document.getElementById('abrir_2');
+	pantalla_1=document.getElementById('pantalla_1');
+	pantalla_2=document.getElementById('pantalla_2');
+	boton_izq=document.getElementById('boton_izq');
+	boton_der=document.getElementById('boton_der');
+	boton_tts=document.getElementById('boton_tts');
+	tts_1=document.getElementById('tts_1');
+	tts_2=document.getElementById('tts_2');
+	contenedor_rango=document.getElementById('contenedor_rango');
+	rango_paginas=document.getElementById('rango_paginas');
+	valor_paginas=document.getElementById('valor_paginas');
+	visor=document.getElementById('visor');
+	contenido=document.getElementById('contenido');
+	modal_tts_ajustes=document.getElementById('modal_tts_ajustes');
+	contenido_tts_ajustes=document.getElementById('contenido_tts_ajustes');
+	rate=document.getElementById('rate');
+	pitch=document.getElementById('pitch');
+	listado_voces_tts=document.getElementById('listado_voces_tts');
+	modal_libros=document.getElementById('modal_libros');
+	listado_libros=document.getElementById('listado_libros');
+	contenido_libros=document.getElementById('contenido_libros');
+	rango_paginas.addEventListener('input',tooltip_range);
 };
 
 /* Manejo db */
-
 function comprobar_base(){
 	nombre_archivo=archivo.name;
 	let datos_libro=db.getItem(nombre_archivo);
@@ -180,13 +115,10 @@ function comprobar_base(){
 		if(confirm("¿Abrir la sección "+(datos_libro[0]+1)+"?")){
 			pagina_cargada=datos_libro[0];
 		};
-	}else{
-		db.setItem(nombre_archivo,'0@0@0');
-	};
+	}else{db.setItem(nombre_archivo,'0@0@0');};
 };
 
 /* Manejo del EPUB */
-
 function solicitar_archivo(){
 	if(!libro_abierto){elem_archivo.click();};
 };
@@ -281,7 +213,7 @@ function procesar_opf(dir_raiz,dir_opf){
 			let temp_elem=file.getElementById(id_elem);
 			dir_paginas.push(dir_raiz+temp_elem['attributes']['href'].nodeValue);
 		};
-		elem_rango_paginas.setAttribute('max',dir_paginas.length-1);
+		rango_paginas.setAttribute('max',dir_paginas.length-1);
 		actualizar_pagina_libro();
 		tooltip_range();
 		cargar_imagenes();
@@ -290,7 +222,6 @@ function procesar_opf(dir_raiz,dir_opf){
 };
 
 /* Manejo de Imanges */
-
 function cargar_imagenes(){
 	if(dir_imagenes[indice_imagenes]!==undefined){
 	zip.file(dir_imagenes[indice_imagenes]['dir']).async('base64')
@@ -305,13 +236,12 @@ function cargar_imagenes(){
 };
 
 /* Manejo de Estilos */
-
 function cargar_estilos(){
 	if(dir_estilos_extra[indice_estilos]!==undefined){
 	zip.file(dir_estilos_extra[indice_estilos]).async('string')
 		.then(function (file){
 			file=file.replace('body','#contenido');
-			elem_estilos_extra.innerHTML+=file;
+			estilos_extra.innerHTML+=file;
 			if(indice_estilos<(dir_estilos_extra.length-1)){
 				indice_estilos+=1;
 				cargar_estilos();
@@ -321,11 +251,10 @@ function cargar_estilos(){
 };
 
 /* Manejo de Paginas */
-
 function cargar_pagina(){
 	if(pagina_cargada!==null){
 		indice_paginas=pagina_cargada;
-		elem_rango_paginas.value=indice_paginas;
+		rango_paginas.value=indice_paginas;
 		tooltip_range();
 		pagina_cargada=null;
 	};
@@ -338,9 +267,9 @@ function cargar_pagina(){
 			file=file.replaceAll(temp_nombre,temp_data);
 		};
 		file=parser.parseFromString(file,'text/html');
-		elem_contenido.innerHTML=file.body.innerHTML;
+		contenido.innerHTML=file.body.innerHTML;
 		libro_abierto=true;
-		let lista_nodos=elem_contenido.children;
+		let lista_nodos=contenido.children;
 		max_parrafos=lista_nodos.length;
 		for(let i=0;i<max_parrafos;i++){
 			lista_nodos[i].setAttribute('id','id_'+i);
@@ -352,7 +281,10 @@ function cargar_pagina(){
 };
 
 function activar_botones(){
-	elem_botones_paginas.classList.toggle('desactivado');
+	boton_izq.classList.remove('desactivado');
+	boton_der.classList.remove('desactivado');
+	contenedor_rango.classList.remove('desactivado');
+	boton_tts.classList.remove('desactivado');
 };
 
 function retroceder_pagina(){
@@ -380,7 +312,7 @@ function avanzar_pagina(){
 };
 
 function cambiar_pagina(){
-	indice_paginas=parseInt(elem_rango_paginas.value,10);
+	indice_paginas=parseInt(rango_paginas.value,10);
 	actualizar_pagina_libro();
 	detener_tts();
 	cambiar_tts_3();
@@ -388,47 +320,45 @@ function cambiar_pagina(){
 };
 
 function actualizar_elem_paginas(){
-	elem_rango_paginas.value=indice_paginas;
+	rango_paginas.value=indice_paginas;
 };
 
 function tooltip_range(){
-	let valor=parseInt(elem_rango_paginas.value);
-	elem_valor_paginas.innerHTML=(valor+1)+' / '+dir_paginas.length;
+	let valor=parseInt(rango_paginas.value);
+	valor_paginas.innerHTML=(valor+1)+' / '+dir_paginas.length;
 };
 
 function mostrar_error(){alert('Ha ocurrido un error al leer el archivo');};
 
 /* Manejo Modales */
-
 function libros(){
 	ocultar_scroll();
 	obtener_registro_libros();
-	elem_modal_libros.style.display='flex';
+	modal_libros.style.display='flex';
 };
 
 function ajustes_tts(){
 	ocultar_scroll();
-	elem_modal_ajustes_tts.style.display='flex';
+	modal_tts_ajustes.style.display='flex';
 };
 
 window.onclick=function(event){
 	switch(event.target){
-		case elem_modal_libros:
-			elem_modal_libros.style.display="none";
+		case modal_libros:
+			modal_libros.style.display="none";
 			habilitar_scroll();
 		break;
-		case elem_modal_ajustes_tts:
-			elem_modal_ajustes_tts.style.display="none";
+		case modal_tts_ajustes:
+			modal_tts_ajustes.style.display="none";
 			habilitar_scroll();
 		break;
 	};
 };
 
 /* Manejo Scroll */
+function habilitar_scroll(){visor.style='overflow:scroll';};
 
-function habilitar_scroll(){elem_visor.style='overflow:scroll';};
-
-function ocultar_scroll(){elem_visor.style='overflow:hidden';};
+function ocultar_scroll(){visor.style='overflow:hidden';};
 
 function maximo_scroll(){
 	habilitar_scroll();
@@ -436,55 +366,38 @@ function maximo_scroll(){
 };
 
 function segundo(){
-	elem_visor.scrollTop=10000000000000000;
-	max_scroll=elem_visor.scrollTop;
-	elem_visor.scrollTop=0;
-};
-
-/* Manejo barra de progreso */
-
-function progreso(){
-	let i=(1/max_scroll)*elem_visor.scrollTop;
-	let porcentaje=(100*i);	
-	elem_valor_progeso.style='min-height:'+porcentaje+'%;'+'max-height:'+porcentaje+'%;';
-};
-
-window.onresize=recalcular_scroll;
-
-function recalcular_scroll(){
-	setTimeout(tercero,200);
-};
-
-function tercero(){
-	let scroll_temp=elem_visor.scrollTop;
-	elem_visor.scrollTop=10000000000000000;
-	max_scroll=elem_visor.scrollTop;
-	elem_visor.scrollTop=scroll_temp;
+	visor.scrollTop=10000000000000000;
+	max_scroll=visor.scrollTop;
+	visor.scrollTop=0;
 };
 
 /* Manejo de la pantalla completa */
-
 function p_completa(){
 	if(document.fullscreenElement===null){
 		if(document.documentElement.requestFullscreen){
 			document.documentElement.requestFullscreen();
-			cambiar_pantalla();
+			pantalla_1.style='display:none';
+			pantalla_2.style='display:flex';
 		}else if(document.documentElement.webkitRequestFullscreen){
 			document.documentElement.webkitRequestFullscreen();
-			cambiar_pantalla();
+			pantalla_1.style='display:none';
+			pantalla_2.style='display:flex';
 		}else if(document.documentElement.msRequestFullscreen){
 			document.documentElement.msRequestFullscreen();
-			cambiar_pantalla();
+			pantalla_1.style='display:none';
+			pantalla_2.style='display:flex';
 		};
 	};
 };
 
 function pantalla_salida(){
-	if(document.fullscreenElement===null){cambiar_pantalla();};
+	if(document.fullscreenElement===null){
+		pantalla_1.style='display:flex';
+		pantalla_2.style='display:none';
+	};
 };
 
 /* Manejo del TTS */
-
 function comprobar_tts(){
 	if(speechSynthesis.onvoiceschanged!==undefined){
 		speechSynthesis.onvoiceschanged=listado_voces;
@@ -515,17 +428,17 @@ function listado_voces(){
 		else if(a_name==b_name){return 0;}
 		else{return +1;}
 	});
-	let indice_voz=elem_listado_voces_tts.selectedIndex<0?0:elem_listado_voces_tts.selectedIndex;
-	elem_listado_voces_tts.innerHTML='';
+	let indice_voz=listado_voces_tts.selectedIndex<0?0:listado_voces_tts.selectedIndex;
+	listado_voces_tts.innerHTML='';
 	for(let i=0;i<voces.length;i++){
 		let elem_opcion=document.createElement('option');
 		elem_opcion.textContent=voces[i].name+' '+voces[i].lang;
 		if(voces[i].default){elem_opcion.textContent+=' -- Prede.';};
 		elem_opcion.setAttribute("data-lang", voces[i].lang);
 		elem_opcion.setAttribute("data-name", voces[i].name);
-		elem_listado_voces_tts.appendChild(elem_opcion);
+		listado_voces_tts.appendChild(elem_opcion);
 	};
-	elem_listado_voces_tts.selectedIndex=indice_voz;
+	listado_voces_tts.selectedIndex=indice_voz;
 };
 
 function iniciar_tts(){
@@ -538,11 +451,11 @@ function leer(){
 		console.error('TTS hablando');
 		return;
 	};
-	if(elem_contenido.textContent!==''){
+	if(contenido.textContent!==''){
 		let elem_temp=document.getElementById('id_'+indice_parrafo);
 		cambiar_seleccionado(elem_temp);
 		let texto=elem_temp.textContent;
-		let voz=elem_listado_voces_tts.selectedOptions[0].getAttribute('data-name');
+		let voz=listado_voces_tts.selectedOptions[0].getAttribute('data-name');
 		for(let i=0;i<voces.length;i++){
 			if (voces[i].name===voz){
 				utterance.voice=voces[i];
@@ -550,8 +463,8 @@ function leer(){
 			};
 		};
 		utterance.text=texto;
-		utterance.pitch=elem_pitch.value;
-		utterance.rate=elem_rate.value;
+		utterance.pitch=pitch.value;
+		utterance.rate=rate.value;
 		tts.speak(utterance);
 	};
 };
@@ -562,7 +475,6 @@ function detener_tts(){
 };
 
 /* Manejo seleccionar parrafo para TTS */
-
 function buscar_padre(){	
 	if(libro_abierto){
 		let elem_hijo=event.target;
@@ -593,43 +505,42 @@ function cambiar_seleccionado(elem_nuevo){
 };
 
 /* Manejo botones encabezado */
-
 function cambiar_abrir(){
-	let temp_boton=document.getElementById('boton_abrir');
-	temp_boton.classList.toggle('abrir');
-	temp_boton.classList.toggle('abrir_2');
-};
-
-function cambiar_pantalla(){
-	let temp_pantalla=document.getElementById('boton_pantalla');
-	temp_pantalla.classList.toggle('pantalla');
-	temp_pantalla.classList.toggle('pantalla_2');
+	boton_abrir.classList.add('desactivado');
+	abrir_1.style='display:none';
+	abrir_2.style='display:flex';
 };
 
 function cambiar_tts(){
-	let temp_tts=document.getElementById('boton_tts');
-	temp_tts.classList.toggle('tts');
-	temp_tts.classList.toggle('tts_2');
+	let temp_valor=tts_1.style.display;
+	if(temp_valor===''){
+		tts_1.style='display:none;';
+		tts_2.style='display:flex;';
+	}else if(temp_valor==='none'){
+		tts_1.style='display:flex;';
+		tts_2.style='display:none;';
+	}else if(temp_valor==='flex'){
+		tts_1.style='display:none;';
+		tts_2.style='display:flex;';
+	}else{
+		console.error('Un valor no permitido:',tts_1.style.display);
+	};
 };
 
 function cambiar_tts_2(){
-	let temp_tts=document.getElementById('boton_tts');
-	temp_tts.classList.remove("tts");
-	temp_tts.classList.add("tts_2");
+	tts_1.style='display:none';
+	tts_2.style='display:flex';
 };
 
 function cambiar_tts_3(){
-	let temp_tts=document.getElementById('boton_tts');
-	temp_tts.classList.remove("tts_2");
-	temp_tts.classList.add("tts");
+	tts_2.style='display:none';
+	tts_1.style='display:flex';
 };
 
 /* Funciones auxilires */
-
 function entero(valor){return Math.floor(valor);};
 
 /* Manejo Modal Libros */
-
 function obtener_registro_libros(){
 	let cant_libros=db.length;
 	let msg='';	
@@ -641,16 +552,20 @@ function obtener_registro_libros(){
 		msg+='<legend class="titulo_listado">'+nombre_libro+'</legend>';
 		msg+='<div class="texto_listado">'+(parseInt(datos_libro[0])+1)+' / '+datos_libro[1]+'</div>';
 		if(datos_libro[2]==='0'){
-			msg+='<div class="boton svg leido" onclick="marcar_leido_libro('+i+')"></div>';
+			msg+='<div class="centrar boton_efecto" onclick="marcar_leido_libro('+i+')">';
+			msg+='<svg class="svg_boton" viewBox="0 0 24 24"><path fill="currentColor" d="M12,20C7.58,20 4,16.42 4,12C4,7.58 7.58,4 12,4C16.42,4 20,7.58 20,12C20,16.42 16.42,20 12,20M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2Z" /></svg>';
+			msg+='</div>';
 		}else if(datos_libro[2]==='1'){
-			msg+='<div class="boton svg no_leido" onclick="marcar_leido_libro('+i+')"></div>';
-		}else{
-			msg+='<div class="boton svg">Error</div>';
-		};
-		msg+='<div class="boton svg borrar" onclick="borrar_entrada_libro('+i+')"></div>';
+			msg+='<div class="centrar boton_efecto" onclick="marcar_leido_libro('+i+')">';
+			msg+='<svg class="svg_boton" viewBox="0 0 24 24"><path fill="currentColor" d="M12,20C7.58,20 4,16.42 4,12C4,7.58 7.58,4 12,4C16.42,4 20,7.58 20,12C20,16.42 16.42,20 12,20M12,2C6.48,2 2,6.48 2,12C2,17.52 6.48,22 12,22C17.52,22 22,17.52 22,12C22,6.48 17.52,2 12,2M12,7C9.24,7 7,9.24 7,12C7,14.76 9.24,17 12,17C14.76,17 17,14.76 17,12C17,9.24 14.76,7 12,7Z" /></svg>';
+			msg+='</div>';
+		}else{msg+='<div class="centrar">Error</div>';};
+		msg+='<div class="centrar boton_efecto" onclick="borrar_entrada_libro('+i+')">';
+		msg+='<svg class="svg_boton" viewBox="0 0 24 24"><path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19C6,20.1 6.9,21 8,21H16C17.1,21 18,20.1 18,19V7H6V19Z" /></svg>';
+		msg+='</div>';
 		msg+='</fieldset>';
 	};
-	elem_listado_libros.innerHTML=msg;
+	listado_libros.innerHTML=msg;
 };
 
 function actualizar_pagina_libro(){
@@ -689,4 +604,13 @@ function borrar_entrada_libro(no_libro){
 			obtener_registro_libros();
 		};
 	}else{console.error('No existe la entrada');};
+};
+
+/* --- */
+function color_fondo(){
+	color_tema+=1;
+	if(color_tema>3){color_tema=1;};
+	document.body.className='tema_'+color_tema;
+	contenido_tts_ajustes.className='grid_modal tema_'+color_tema;
+	contenido_libros.className='tarjeta_modal tema_'+color_tema;
 };
